1ef528daa3cde1ef9b1572723576c9a3b9a9708a
shadreza <45334134+shadreza@users.noreply.github.com>
2021-06-20 02:39:18 +0600

    will have to work on the employee working page

---
+++ src/Components/EmployeeWorks/EmployeeWorks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EmployeeWorks.css';
+
+const EmployeeWorks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmployeeWorks;
